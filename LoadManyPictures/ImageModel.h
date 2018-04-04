4351930aef2f7947d193ad00f5666f26e39bc24f
yuxinwen <yuxinwen@lvmama.com>
2018-04-04 18:06:45 +0800

    loadmanypicture

---
+++ LoadManyPictures/ImageModel.h
@@ -0,0 +1,13 @@
+//
+//  ImageModel.h
+//  LoadManyPictures
+//
+//  Created by 新闻 on 2018/4/4.
+//  Copyright © 2018年 Lvmama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageModel : NSObject
+
+@end
---
+++ LoadManyPictures/ImageModel.m
@@ -0,0 +1,13 @@
+//
+//  ImageModel.m
+//  LoadManyPictures
+//
+//  Created by 新闻 on 2018/4/4.
+//  Copyright © 2018年 Lvmama. All rights reserved.
+//
+
+#import "ImageModel.h"
+
+@implementation ImageModel
+
+@end
